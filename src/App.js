928800099def72949ea1cb35e02afc6b73a36009
alezi-n <study.alez@gmail.com>
2023-12-28 00:31:36 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+// import { DivCabecalho } from "./componentes/Cabecalho/Cabecalho";
+import { CampoTexto } from "./componentes/CampoTexto/CampoTexto";
 import { Card } from "./componentes/Card/Card";
 import { Estilos } from "./componentes/EstilosGlobais/Estilos";
 import { ProvedorTema } from "./componentes/ProvedorTema/ProvedorTema";
+import { Tipografia } from "./componentes/Tipografia/Tipografia";
 
 function App() {
   return (
     <ProvedorTema>
       <Estilos />
-
       <Card>
-        <h1>Freelando</h1>
+        <Tipografia variante="h3" componente="h1">
+          Freelando
+        </Tipografia>
+        <Tipografia variante="body" componente="body">
+        Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho. 
+        </Tipografia>
+        <CampoTexto titulo="Nome Completo"></CampoTexto>
       </Card>
     </ProvedorTema>
   );
---
+++ src/componentes/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,12 @@
+// import styled from '@emotion/styled'
+
+// export const DivCabecalho = styled.header`
+//     background: ${props => props.theme.cores.primarias.a};
+//     padding: ${props => props.theme.espacamentos.m};
+// `
+
+// export const Cabecalho = ({ children }) => {
+//     return (<DivCabecalho>
+//         {children}
+//     </DivCabecalho>)
+// }
